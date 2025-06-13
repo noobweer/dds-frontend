4e4360601c540e1346032a3b0a13128337c43311
noobweer <mokrowon@gmail.com>
2025-06-13 23:45:38 +1000

    ⚙️ setup of project

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import PrimeVue from 'primevue/config'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  unstyled: true,
+})
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
